cf5ae92da5eb677aad2c79a06b2ec9bf7877c1cf
Ewa Czechowska <ewa@ai-traders.com>
2016-02-05 22:12:05 +0100

    #8264 blacklisting and ignoring some variables

--- test/shpec/ide.sh
+++ test/shpec/ide.sh
@@ -5,7 +5,7 @@ describe "commandline options"
   describe "--version"
     it "outputs the current version number"
       message="$(${IDE_PATH} --version)"
-      assert equal "$message" "${IDE_PATH} version 0.0.1"
+      assert equal "$message" "${IDE_PATH} version 0.1.0"
     end
   end
   describe "--group"
