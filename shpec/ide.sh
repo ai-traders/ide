d35458c6f6ec73d9421d370e8d464d3e72ddadea
Ewa Czechowska <ewa@ai-traders.com>
2016-02-06 15:27:50 +0100

    fixed some shpec tests

--- shpec/ide.sh
+++ shpec/ide.sh
@@ -23,7 +23,7 @@ describe "commandline options"
     it "exits 1, if --idefile not set and does not exist in curent directory"
       message="$(${IDE_PATH} --dryrun some_command)"
       assert equal "$?" "1"
-      assert equal "$message" "idefile: ${PWD}/Idefile does not exist"
+      assert do_match "$message" "idefile: ${PWD}/Idefile does not exist"
     end
     it "exits 1, if zero-length string set"
       # do not use \"\" it will not be counted as empty string
@@ -43,45 +43,44 @@ describe "commandline options"
     end
   end
   describe "command"
-    it "exits 1, if zero-length command set"
+    it "exits 0, if zero-length command set"
       # do not use \"\" it will not be counted as empty string
-      message="$(cd examples/gitide && ${IDE_PATH} --idefile examples/gitide/Idefile --dryrun)"
-      assert equal "$?" "1"
-      assert equal "$message" "command not specified"
+      message="$(cd examples/gitide && ${IDE_PATH} --dryrun)"
+      assert equal "$?" "0"
     end
     it "exits 0, if not zero-length command set"
       message="$(cd examples/gitide && ${IDE_PATH} --dryrun some_command)"
       assert equal "$?" "0"
     end
   end
-#   describe "docker run command, using gitide"
-#     it "exits 0, constructs correct command"
-#       # do not use \"\" it will not be counted as empty string
-#       message="$(cd examples/gitide && IDE_LOG_LEVEL=debug ${IDE_PATH} --dryrun some_command)"
-#       assert equal "$?" "0"
-#       assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/examples/gitide/work:/ide/work\ -v\ ${HOME}:/ide/identity:ro\ gitide:0.1.0\ \\\"some_command\ \\\""
-#     end
-#     it "exits 0, constructs correct command longer"
-#       # do not use \"\" it will not be counted as empty string
-#       message="$(cd examples/gitide && IDE_LOG_LEVEL=debug ${IDE_PATH} --dryrun some_command longer)"
-#       assert equal "$?" "0"
-#       assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/examples/gitide/work:/ide/work\ -v\ ${HOME}:/ide/identity:ro\ gitide:0.1.0\ \\\"some_command longer\ \\\""
-#     end
-#     it "exits 0, constructs correct command longer longer"
-#       # do not use \"\" it will not be counted as empty string
-#       message="$(cd examples/gitide && IDE_LOG_LEVEL=debug ${IDE_PATH} --dryrun some_command longer longer)"
-#       assert equal "$?" "0"
-#       assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/examples/gitide/work:/ide/work\ -v\ ${HOME}:/ide/identity:ro\ gitide:0.1.0\ \\\"some_command longer longer\ \\\""
-#     end
-#   end
-#   describe "docker run command, using complexide"
-#     it "exits 0, constructs correct command"
-#       # do not use \"\" it will not be counted as empty string
-#       message="$(IDE_LOG_LEVEL=debug ${IDE_PATH} --idefile test/complexide-usage/Idefile --dryrun some_command)"
-#       assert equal "$?" "0"
-#       assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/test/empty_work_dir:/ide/work\ -v\ ${PWD}/test/empty_home_dir:/ide/identity:ro\ -e\ ABC=1\ -e\ DEF=2\ -e\ GHI=3\ --privileged\ complexide:0.1.0\ \\\"some_command\ \\\""
-#     end
-#   end
+  describe "docker run command, using gitide"
+    it "exits 0, constructs correct command"
+      # do not use \"\" it will not be counted as empty string
+      message="$(cd examples/gitide && IDE_LOG_LEVEL=debug ${IDE_PATH} --dryrun some_command)"
+      assert equal "$?" "0"
+      assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/examples/gitide/work:/ide/work\ -v\ ${HOME}:/ide/identity:ro\ gitide:0.1.0\ \\\"some_command\ \\\""
+    end
+    it "exits 0, constructs correct command longer"
+      # do not use \"\" it will not be counted as empty string
+      message="$(cd examples/gitide && IDE_LOG_LEVEL=debug ${IDE_PATH} --dryrun some_command longer)"
+      assert equal "$?" "0"
+      assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/examples/gitide/work:/ide/work\ -v\ ${HOME}:/ide/identity:ro\ gitide:0.1.0\ \\\"some_command\ longer\\\""
+    end
+    it "exits 0, constructs correct command longer longer"
+      # do not use \"\" it will not be counted as empty string
+      message="$(cd examples/gitide && IDE_LOG_LEVEL=debug ${IDE_PATH} --dryrun some_command longer longer)"
+      assert equal "$?" "0"
+      assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/examples/gitide/work:/ide/work\ -v\ ${HOME}:/ide/identity:ro\ gitide:0.1.0\ \\\"some_command\ longer\ longer\\\""
+    end
+  end
+  # describe "docker run command, using complexide"
+  #   it "exits 0, constructs correct command"
+  #     # do not use \"\" it will not be counted as empty string
+  #     message="$(IDE_LOG_LEVEL=debug ${IDE_PATH} --idefile test/complexide-usage/Idefile --dryrun some_command)"
+  #     assert equal "$?" "0"
+  #     assert match "$message" "docker\ run\ --rm\ -v\ ${PWD}/test/empty_work_dir:/ide/work\ -v\ ${PWD}/test/empty_home_dir:/ide/identity:ro\ -e\ ABC=1\ -e\ DEF=2\ -e\ GHI=3\ --privileged\ complexide:0.1.0\ \\\"some_command\ \\\""
+  #   end
+  # end
 #   describe "docker run command, using invalid-driver-ide"
 #     it "exits 1"
 #       # do not use \"\" it will not be counted as empty string
--- shpec/matchers/do_match.sh
+++ shpec/matchers/do_match.sh
@@ -3,11 +3,13 @@
 do_match() {
   actual=$1
   expected=$2
+  echo "aaaaaaaaa"
+  echo $actual
+  echo $expected
+  # http://askubuntu.com/questions/299710/how-to-determine-if-a-string-is-a-substring-of-another-in-bash
   if case ${actual} in *"${expected}"*) true;; *) false;; esac; then
-      printf %s\\n "'${actual}' contains '${expected}'"
       iecho "$_shpec_green$_shpec_assertion$_shpec_norm"
   else
-      printf %s\\n "'${actual}' does not contain '${expected}'"
       : $((_shpec_failures += 1))
            iecho "$_shpec_red$_shpec_assertion"
            iecho "${actual}' does not contain '${expected}$_shpec_norm"
