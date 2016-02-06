e56ef1886636398cf7c0e759167d30de47bd51e7
Ewa Czechowska <ewa@ai-traders.com>
2016-02-06 22:49:01 +0100

    #8264 "compgen -e" to list all the env variables

--- shpec/ide_save_environment_variables.sh
+++ shpec/ide_save_environment_variables.sh
@@ -1,12 +1,15 @@
-# describe "commandline options"
-#   # make absolute path out of relative
-#   IDE_PATH=$(readlink -f "./ide_functions")
-#   env_file = 'tmp_env'
-#
-#   describe 'get_env_vars_file_name'
-#     it "returns file name"
-#       message="source ${IDE_PATH} && save_environment_variables #{env_file}"
-#       assert equal "$message" "/tmp/ide/environment-"
-#     end
-#   end
-# end
+describe "commandline options"
+  # make absolute path out of relative
+  IDE_PATH=$(readlink -f "./ide_functions")
+  env_file='tmp_env'
+
+  describe 'preserves correct variables'
+    it "returns file name"
+      message="$(/bin/bash -c "source ${IDE_PATH} && DUMMY=123 save_environment_variables ${env_file}")"
+      assert equal "$?" "0"
+      file_contents=$(cat ${env_file})
+      assert match "$file_contents" "DUMMY=123"
+      assert match "$message" "aaab"
+    end
+  end
+end
--- shpec/matchers/do_match.sh
+++ shpec/matchers/do_match.sh
@@ -3,9 +3,6 @@
 do_match() {
   actual=$1
   expected=$2
-  echo "aaaaaaaaa"
-  echo $actual
-  echo $expected
   # http://askubuntu.com/questions/299710/how-to-determine-if-a-string-is-a-substring-of-another-in-bash
   if case ${actual} in *"${expected}"*) true;; *) false;; esac; then
       iecho "$_shpec_green$_shpec_assertion$_shpec_norm"
