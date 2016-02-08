45d7642d7867aaf942a9cfd119e6ea81b2590401
Ewa Czechowska <ewa@ai-traders.com>
2016-02-08 19:40:04 +0100

    color in entrypoint in gitide

--- examples/gitide/docker/scripts/entrypoint.sh
+++ examples/gitide/docker/scripts/entrypoint.sh
@@ -10,17 +10,18 @@ set -e
 /usr/bin/ide-fix-uid-gid.sh
 
 source /etc/docker_metadata.txt
-
+GREEN='\033[0;32m'
+NC='\033[0m'
 if [ -t 0 ] ; then
     # interactive shell
-    echo "ide init finished (interactive shell), using ${this_image_name}:${this_image_tag}"
+    echo -e "${GREEN}ide init finished (interactive shell), using ${this_image_name}:${this_image_tag}${NC}"
 
     # No "set -e" here, you don't want to be logged out when sth returns not 0
     # in interactive shell.
     set +e
 else
     # not interactive shell
-    echo "ide init finished (not interactive shell), using ${this_image_name}:${this_image_tag}"
+    echo -e "${GREEN}ide init finished (not interactive shell), using ${this_image_name}:${this_image_tag}${NC}"
     set -e
 fi
 
