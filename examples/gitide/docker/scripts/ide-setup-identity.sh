6145af44f53e10b770449ebd7e12d0e344e63ccb
Ewa Czechowska <ewa@ai-traders.com>
2016-02-08 20:01:49 +0100

    copy first the whole .ssh dir, then generate any configs

--- examples/gitide/docker/scripts/ide-setup-identity.sh
+++ examples/gitide/docker/scripts/ide-setup-identity.sh
@@ -9,27 +9,28 @@
 # From that directory we mapt configuration and secrets files.
 ide_identity="/ide/identity"
 
-# 1st file we need
-# at least this is needed:
+# 1st directory we need, copy it with all the secrets, particulary id_rsa
+if [ ! -d "$ide_identity/.ssh" ]; then
+  echo "$ide_identity/.ssh does not exist"
+  exit 1;
+fi
+if [ ! -f "$ide_identity/.ssh/id_rsa" ]; then
+  echo "$ide_identity/.ssh/id_rsa does not exist"
+  exit 1;
+fi
+cp -r "$ide_identity/.ssh" "/home/ide/"
+
+# 2nd we need to ensure that /home/ide/.ssh/config contains at least:
 # StrictHostKeyChecking no
 if [ ! -f "$ide_identity/.ssh/config" ]; then
   echo "$ide_identity/.ssh/config does not exist"
   exit 1;
 fi
-mkdir -p /home/ide/.ssh
-touch /home/ide/.ssh/config
+rm /home/ide/.ssh/config
 cat $ide_identity/.ssh/config | sed -E 's/IdentityFile.*/IdentityFile \/home\/ide\/.ssh\/id_rsa/g' >> /home/ide/.ssh/config
-
 # actually, a better idea, would be to generate /home/ide/.ssh/config here
 # instead of copying it; same for other configs which need user name or other
 # user specific variables, e.g. knife.rb for chef
-
-# 2nd file we need
-if [ ! -f "$ide_identity/.ssh/id_rsa" ]; then
-  echo "$ide_identity/.ssh/id_rsa does not exist"
-  exit 1;
-fi
-cp $ide_identity/.ssh/id_rsa /home/ide/.ssh/
 
 # 3rd: not obligatory configuration file
 if [ -f "$ide_identity/.gitconfig" ]; then
@@ -38,5 +39,7 @@ fi
 
 # 4rd file we need; in order to ensure that after bash login, the ide user
 # is in /ide/work. Not obligatory but shortens end user's commands.
+# Do not copy it from $IDE_IDENTITY, because it may reference sth not installed in
+# this docker image.
 touch /home/ide/.profile
 echo "cd /ide/work" > /home/ide/.profile
