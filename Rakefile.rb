2bed28c5fcadd68548f7cc44b5a98590e0ae45c2
Ewa Czechowska <ewa@ai-traders.com>
2016-02-09 14:27:31 +0000

    #8733 no bundler needed for rake tasks like shellcheck

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,4 +1,5 @@
-# only those tasks need additional ruby dependencies (gems)
+# only those tasks need additional ruby dependencies (gems),
+# bundler must be installed
 rule(/.*repocritic|.*release:code|.*validate_repo/) do |task|
   ENV['BUNDLE_GEMFILE'] = File.expand_path("#{File.dirname(__FILE__)}/Gemfile")
   unless ENV['SKIP_ALL_DEPENDENCIES'] || ENV['SKIP_RUBY_DEPENDENCIES']
@@ -10,9 +11,10 @@ rule(/.*repocritic|.*release:code|.*validate_repo/) do |task|
   sh "bundle exec rake #{task.name} -f #{inner_rakefile}"
 end
 
-# any other rake task
+# any other rake task should be run without bundler, because no ruby dependencies
+# are needed
 rule(//) do |task|
   inner_rakefile = File.expand_path(
     "#{File.dirname(__FILE__)}/InnerRakefile.rb")
-  sh "bundle exec rake #{task.name} -f #{inner_rakefile}"
+  sh "rake #{task.name} -f #{inner_rakefile}"
 end
