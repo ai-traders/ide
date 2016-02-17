4617144fc66379c75ac52b705d631659c4acb026
Ewa Czechowska <ewa@ai-traders.com>
2016-02-17 16:47:48 +0000

    #8783 save environment variables to a file containing random number

--- shpec/ide_get_env_vars_file_name.sh
+++ shpec/ide_get_env_vars_file_name.sh
@@ -5,7 +5,9 @@ describe "get_env_vars_file_name"
   describe 'get_env_vars_file_name'
     it "returns file name"
       message="$(/bin/bash -c "source ${IDE_PATH} && get_env_vars_file_name")"
-      assert match "$message" "/tmp/ide/environment-"
+      # e.g. /tmp/ide/environment-2016-02-17_16-32-29-68192406
+      # {2,} == match at least 2 occurrences of a char
+      assert do_match "$message" "/tmp/ide/environment-[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2,}"
     end
   end
 end
