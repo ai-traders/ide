98ae7c6c32fb57d8b00b1116b7521a3b9b6eda6b
Ewa Czechowska <ewa@ai-traders.com>
2016-03-08 22:12:09 +0100

    let it work in ideide

--- shpec/ide_get_run_id.sh
+++ shpec/ide_get_run_id.sh
@@ -7,7 +7,8 @@ describe "get_run_id and similar"
       message="$(/bin/bash -c "source ${IDE_PATH} && get_run_id")"
       # e.g. ide-gitide-usage-2016-03-08_18-51-09-68509321
       # {2,} == match at least 2 occurrences of a char
-      assert do_match "$message" "ide-ide-[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2,}"
+      assert do_match "$message" "ide-"
+      assert do_match "$message" "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2,}"
     end
   end
   describe 'get_env_vars_file_name'
